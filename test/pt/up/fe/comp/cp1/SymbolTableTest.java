3e3edf33041ba33da7cdc0fc564fc99ef31e3967
mariajvieira <up202204802@up.pt>
2025-03-26 00:05:55 +0000

    more symbol table tests

--- test/pt/up/fe/comp/cp1/SymbolTableTest.java
+++ test/pt/up/fe/comp/cp1/SymbolTableTest.java
@@ -183,14 +183,12 @@ public class SymbolTableTest {
     @Test
     public void fullSymbolTableDeclaredClassTest() {
         var st = test("symboltable/FullTest.jmm", false).getSymbolTable();
-        // Declared class
         assertEquals("FullTest", st.getClassName());
     }
 
     @Test
     public void fullSymbolTableFieldsTest() {
         var st = test("symboltable/FullTest.jmm", false).getSymbolTable();
-        // Fields with type and array information
         List<Symbol> fields = st.getFields();
         assertEquals(3, fields.size());
         var field1 = fields.stream().filter(s -> s.getName().equals("field1")).findFirst().orElse(null);
@@ -210,12 +208,11 @@ public class SymbolTableTest {
     @Test
     public void fullSymbolTableMethodsTest() {
         var st = test("symboltable/FullTest.jmm", false).getSymbolTable();
-        // Methods in the declared class
         List<String> methods = st.getMethods();
         assertEquals(2, methods.size());
         assertTrue(methods.contains("method1"));
         assertTrue(methods.contains("method2"));
-        // Parameters and return type for method1
+
         Type retMethod1 = st.getReturnType("method1");
         assertEquals("int", retMethod1.getName());
         assertFalse(retMethod1.isArray());
@@ -228,7 +225,6 @@ public class SymbolTableTest {
         assertNotNull(paramB);
         assertEquals("boolean", paramB.getType().getName());
 
-        // Parameters and return type for method2
         Type retMethod2 = st.getReturnType("method2");
         assertEquals("boolean", retMethod2.getName());
         List<Symbol> paramsMethod2 = st.getParameters("method2");
@@ -241,7 +237,6 @@ public class SymbolTableTest {
     @Test
     public void fullSymbolTableLocalsTest() {
         var st = test("symboltable/FullTest.jmm", false).getSymbolTable();
-        // Local variables for method1
         List<Symbol> localsMethod1 = st.getLocalVariables("method1");
         assertEquals(2, localsMethod1.size());
         var local1 = localsMethod1.stream().filter(s -> s.getName().equals("local1")).findFirst().orElse(null);
@@ -251,7 +246,6 @@ public class SymbolTableTest {
         assertNotNull(local2);
         assertEquals("boolean", local2.getType().getName());
 
-        // Local variables for method2
         List<Symbol> localsMethod2 = st.getLocalVariables("method2");
         assertEquals(1, localsMethod2.size());
         var local3 = localsMethod2.stream().filter(s -> s.getName().equals("local3")).findFirst().orElse(null);
@@ -291,6 +285,83 @@ public class SymbolTableTest {
         assertEquals(new Type("void", false), st.getReturnType("voidMethod"));
         assertEquals(new Type("int", true), st.getReturnType("arrayMethod"));
         assertEquals(new Type("ReturnTypes", false), st.getReturnType("objectMethod"));
+    }
+
+    @Test
+    public void testDeclaredClass() {
+        var st = test("symboltable/FullTest.jmm", false).getSymbolTable();
+        assertEquals("Declared class name", "FullTest", st.getClassName());
+        assertNull("Super class should be null", st.getSuper());
+    }
+
+    @Test
+    public void testFields() {
+        var st = test("symboltable/FullTest.jmm", false).getSymbolTable();
+        List<Symbol> fields = st.getFields();
+        assertEquals("Number of fields", 3, fields.size());
+
+        var field1 = fields.stream().filter(s -> s.getName().equals("field1")).findFirst().orElse(null);
+        assertNotNull("Field field1 is missing", field1);
+        assertEquals("Type for field1", "int", field1.getType().getName());
+        assertFalse("field1 is not an array", field1.getType().isArray());
+
+        var field2 = fields.stream().filter(s -> s.getName().equals("field2")).findFirst().orElse(null);
+        assertNotNull("Field field2 is missing", field2);
+        assertEquals("Type for field2", "boolean", field2.getType().getName());
+        assertFalse("field2 is not an array", field2.getType().isArray());
+
+        var fieldArr = fields.stream().filter(s -> s.getName().equals("fieldArr")).findFirst().orElse(null);
+        assertNotNull("Field fieldArr is missing", fieldArr);
+        assertEquals("Type for fieldArr", "int", fieldArr.getType().getName());
+        assertTrue("fieldArr is an array", fieldArr.getType().isArray());
+    }
+
+    @Test
+    public void testMethodsAndParameters() {
+        var st = test("symboltable/FullTest.jmm", false).getSymbolTable();
+        List<String> methods = st.getMethods();
+        assertEquals("Number of methods", 2, methods.size());
+        assertTrue("Method method1 not found", methods.contains("method1"));
+        assertTrue("Method method2 not found", methods.contains("method2"));
+
+        Type retMethod1 = st.getReturnType("method1");
+        assertEquals("Return type of method1", "int", retMethod1.getName());
+        assertFalse("Return type of method1 should not be an array", retMethod1.isArray());
+        List<Symbol> paramsMethod1 = st.getParameters("method1");
+        assertEquals("Number of parameters for method1", 2, paramsMethod1.size());
+        var paramA = paramsMethod1.stream().filter(s -> s.getName().equals("a")).findFirst().orElse(null);
+        var paramB = paramsMethod1.stream().filter(s -> s.getName().equals("b")).findFirst().orElse(null);
+        assertNotNull("Parameter a for method1 missing", paramA);
+        assertEquals("Parameter a type", "int", paramA.getType().getName());
+        assertNotNull("Parameter b for method1 missing", paramB);
+        assertEquals("Parameter b type", "boolean", paramB.getType().getName());
+
+        Type retMethod2 = st.getReturnType("method2");
+        assertEquals("Return type of method2", "boolean", retMethod2.getName());
+        List<Symbol> paramsMethod2 = st.getParameters("method2");
+        assertEquals("Number of parameters for method2", 1, paramsMethod2.size());
+        var paramC = paramsMethod2.stream().filter(s -> s.getName().equals("c")).findFirst().orElse(null);
+        assertNotNull("Parameter c for method2 missing", paramC);
+        assertEquals("Parameter c type", "boolean", paramC.getType().getName());
+    }
+
+    @Test
+    public void testLocals() {
+        var st = test("symboltable/FullTest.jmm", false).getSymbolTable();
+        List<Symbol> localsMethod1 = st.getLocalVariables("method1");
+        assertEquals("Number of locals for method1", 2, localsMethod1.size());
+        var local1 = localsMethod1.stream().filter(s -> s.getName().equals("local1")).findFirst().orElse(null);
+        var local2 = localsMethod1.stream().filter(s -> s.getName().equals("local2")).findFirst().orElse(null);
+        assertNotNull("Local local1 for method1 missing", local1);
+        assertEquals("Type for local1", "int", local1.getType().getName());
+        assertNotNull("Local local2 for method1 missing", local2);
+        assertEquals("Type for local2", "boolean", local2.getType().getName());
+
+        List<Symbol> localsMethod2 = st.getLocalVariables("method2");
+        assertEquals("Number of locals for method2", 1, localsMethod2.size());
+        var local3 = localsMethod2.stream().filter(s -> s.getName().equals("local3")).findFirst().orElse(null);
+        assertNotNull("Local local3 for method2 missing", local3);
+        assertEquals("Type for local3", "boolean", local3.getType().getName());
     }
 
 
